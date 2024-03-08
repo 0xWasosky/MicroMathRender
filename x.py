74707517e90a1b6ebe05f73daa13f3dd60dc51c1
Wasosky <110636486+0xWasosky@users.noreply.github.com>
2024-03-08 20:48:33 +0100

    Add files via upload

---
+++ x.py
@@ -0,0 +1,26 @@
+import json
+import pyglet
+from pyglet import shapes
+
+
+config = json.load(open("configs.json", 'r'))
+
+X = config['x']
+Y = config['y']
+
+window = pyglet.window.Window(X, Y)
+batch = pyglet.graphics.Batch()
+
+
+y = shapes.Line(x=X // 2, y=0, x2=X // 2, y2=Y, batch=batch)
+x = shapes.Line(x=0, y=Y // 2, x2=X, y2=Y // 2, batch=batch)
+
+
+line = shapes.Line(x=X, y=Y, x2=0, y2=0, batch=batch)\
+
+@window.event
+def on_draw():
+    window.clear()
+    batch.draw()
+
+pyglet.app.run()
---
+++ x_abs.py
@@ -0,0 +1,27 @@
+import json
+import pyglet
+from pyglet import shapes
+
+
+config = json.load(open("configs.json", 'r'))
+
+X = config['x']
+Y = config['y']
+
+window = pyglet.window.Window(X, Y)
+batch = pyglet.graphics.Batch()
+
+
+y = shapes.Line(x=X // 2, y=0, x2=X // 2, y2=Y, batch=batch)
+x = shapes.Line(x=0, y=Y // 2, x2=X, y2=Y // 2, batch=batch)
+
+
+line = shapes.Line(x=X // 2, y=Y // 2, x2=X, y2=Y, batch=batch)
+line2 = shapes.Line(x=0, y=Y, x2=X // 2, y2=Y // 2, batch=batch)
+
+@window.event
+def on_draw():
+    window.clear()
+    batch.draw()
+
+pyglet.app.run()
